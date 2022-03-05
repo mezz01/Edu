3d26ddc7879dbd67ca91b9afd1e89949a6795a22
mezzat01 <mezzatamine2002@gmail.com>
2022-03-05 18:11:05 +0100

    mobile version

--- main.js
+++ main.js
@@ -6,5 +6,23 @@ window.addEventListener('scroll', () => {
 })
 
 
+// menu open/close
+//open :
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu");
+const closeBtn = document.querySelector("#close-menu");
+
+menuBtn.addEventListener('click', () => {
+      menu.style.display = "flex";
+      closeBtn.style.display = "inline-block";
+      menuBtn.style.display = "none"
+})
 
 
+//close:
+const closeNav = () => {
+      menu.style.display = "none";
+      closeBtn.style.display = "none";
+      menuBtn.style.display = "inline-block"
+}
+closeBtn.addEventListener('click', closeNav);
